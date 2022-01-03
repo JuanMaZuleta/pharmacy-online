a78bb110f3555ff565e95b8e020b9c61e2a7d8a9
Pedro Martinez <pedroomtz17@gmail.com>
2022-01-03 14:52:19 -0600

    creacion de script de secuencias

---
+++ s-05-secuencias.sql
@@ -0,0 +1,27 @@
+create sequence seq_empleado 
+start with 1
+increment by 3
+nomaxvalue
+nominvalue
+nocycle
+cache 6;
+
+create sequence seq_ubicacion
+
+
+create sequence seq_pedido
+
+
+create sequence seq_tarjeta
+
+
+create sequence seq_cliente
+
+
+create sequence seq_status_pedido
+
+
+create sequence seq_historico_status
+
+
+create sequence seq_detalle_pedido
