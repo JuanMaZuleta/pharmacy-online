a9c770e7f397787ae5375a89c2c57dcfadcc9e84
Pedro Martinez <pedroomtz17@gmail.com>
2022-01-03 17:15:20 -0600

    creacion de script s-01-usuarios

---
+++ s-01-usuarios.sql
@@ -0,0 +1,59 @@
+--@Autor(es):  Cruz Ramos Diego Alejandro
+--             Martinez Alvarez Pedro
+--             Zuleta Ceja Juan Manuel
+--@Fecha creación: 03/01/2022
+--@Descripción: Creaciòn de usuarios y roles
+
+set serveroutput on
+declare
+v_count number(1,0);
+begin
+select count(*) into v_count
+from dba_users
+where username ='CMZ_PROY_ADMIN';
+if v_count > 0 then
+execute immediate 'drop user CMZ_PROY_ADMIN cascade';
+dbms_output.put_line('Usuario CMZ_PROY_ADMIN eliminado');
+else
+dbms_output.put_line('El usuario CMZ_PROY_ADMIN no existe, no se requiere eliminar');
+end if;
+end;
+/
+
+set serveroutput on
+declare
+v_count number(1,0);
+begin
+select count(*) into v_count
+from dba_users
+where username ='CMZ_PROY_INVITADO';
+if v_count > 0 then
+execute immediate 'drop user CMZ_PROY_INVITADO cascade';
+dbms_output.put_line('Usuario CMZ_PROY_INVITADO eliminado');
+else
+dbms_output.put_line('El usuario CMZ_PROY_INVITADO no existe, no se requiere eliminar');
+end if;
+end;
+/
+
+--Creación de roles. 
+prompt creando roles
+
+create role rol_admin;
+grant create session, create table, create view,create procedure, 
+create sequence to rol_admin;
+
+create role rol_invitado;
+grant create session to rol_invitado;
+
+--Crear usuarios
+prompt creando usuarios
+
+create user cmz_proy_admin identified by cmz quota 1024M on users;
+grant rol_admin to cmz_proy_admin;
+
+create user cmz_proy_invitado identified by cmz;
+grant rol_invitado to cmz_proy_invitado;
+
+Prompt Listo!
+disconnect;
