a2baac8a8d1f4c91b3c6dc1845cf437b25571071
Pedro Martinez <pedroomtz17@gmail.com>
2022-01-04 17:04:12 -0600

    agregacion de secuencias

--- s-05-secuencias.sql
+++ s-05-secuencias.sql
@@ -1,3 +1,9 @@
+--@Autor(es):  Cruz Ramos Diego Alejandro
+--             Martinez Alvarez Pedro
+--             Zuleta Ceja Juan Manuel
+--@Fecha creación: 04/01/2022
+--@Descripción: Creaciòn de secuencias
+
 create sequence seq_empleado 
 start with 1
 increment by 3
@@ -14,7 +20,6 @@ nominvalue
 nocycle
 cache 6;
 
-
 create sequence seq_pedido
 start with 1
 increment by 3
@@ -22,7 +27,6 @@ nomaxvalue
 nominvalue
 nocycle
 cache 6;
-
 
 create sequence seq_tarjeta
 start with 1
@@ -40,16 +44,24 @@ nominvalue
 nocycle
 cache 6;
 
-
 create sequence seq_status_pedido
 start with 1
 increment by 1
-maxvalue  
-minvalue
+maxvalue 10
+minvalue 1
 nocycle;
 
-
 create sequence seq_historico_status
-
+start with 1
+increment by 1
+maxvalue 50
+minvalue 1
+nocycle;
 
 create sequence seq_detalle_pedido
+start with 1
+increment by 3
+nomaxvalue
+nominvalue
+nocycle
+cache 6;
