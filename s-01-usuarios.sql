24db1e11c90081a352fbf004e19eea12004714b6
pedroalvarez <pedro@pc-pma.fi.unam>
2022-01-04 16:11:00 -0600

    parte del codigo se paso al script s-00-main.sql

--- s-01-usuarios.sql
+++ s-01-usuarios.sql
@@ -4,38 +4,6 @@
 --@Fecha creación: 03/01/2022
 --@Descripción: Creaciòn de usuarios y roles
 
-set serveroutput on
-declare
-v_count number(1,0);
-begin
-select count(*) into v_count
-from dba_users
-where username ='CMZ_PROY_ADMIN';
-if v_count > 0 then
-execute immediate 'drop user CMZ_PROY_ADMIN cascade';
-dbms_output.put_line('Usuario CMZ_PROY_ADMIN eliminado');
-else
-dbms_output.put_line('El usuario CMZ_PROY_ADMIN no existe, no se requiere eliminar');
-end if;
-end;
-/
-
-set serveroutput on
-declare
-v_count number(1,0);
-begin
-select count(*) into v_count
-from dba_users
-where username ='CMZ_PROY_INVITADO';
-if v_count > 0 then
-execute immediate 'drop user CMZ_PROY_INVITADO cascade';
-dbms_output.put_line('Usuario CMZ_PROY_INVITADO eliminado');
-else
-dbms_output.put_line('El usuario CMZ_PROY_INVITADO no existe, no se requiere eliminar');
-end if;
-end;
-/
-
 --Creación de roles. 
 prompt creando roles
 
@@ -56,12 +24,3 @@ create user cmz_proy_invitado identified by cmz;
 grant rol_invitado to cmz_proy_invitado;
 
 
-prompt conectando como cmz_proy_admin
-connect cmz_proy_admin/cmz
-
-prompt ejecuciòn de s-02
-
-
-prompt ejecuciòn de s-03-carga-inicial.sql
-
-Prompt Listo!
