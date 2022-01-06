9f0ed8e33f763f5236df2cd30daa65ec18421065
Pedro Martinez <pedroomtz17@gmail.com>
2022-01-06 13:10:17 -0600

    corrección de identación

--- s-00-main.sql
+++ s-00-main.sql
@@ -6,32 +6,32 @@
 
 set serveroutput on
 declare
-v_count number(1,0);
+  v_count number(1,0);
 begin
-select count(*) into v_count
-from dba_users
-where username ='CMZ_PROY_ADMIN';
-if v_count > 0 then
-execute immediate 'drop user CMZ_PROY_ADMIN cascade';
-dbms_output.put_line('Usuario CMZ_PROY_ADMIN eliminado');
-else
-dbms_output.put_line('El usuario CMZ_PROY_ADMIN no existe, no se requiere eliminar');
-end if;
+  select count(*) into v_count
+  from dba_users
+  where username ='CMZ_PROY_ADMIN';
+  if v_count > 0 then
+    execute immediate 'drop user CMZ_PROY_ADMIN cascade';
+    dbms_output.put_line('Usuario CMZ_PROY_ADMIN eliminado');
+  else
+    dbms_output.put_line('El usuario CMZ_PROY_ADMIN no existe, no se requiere eliminar');
+  end if;
 end;
 /
 
 set serveroutput on
-declare
-v_count number(1,0);
+declare 
+  v_count number(1,0);
 begin
-select count(*) into v_count
-from dba_users
-where username ='CMZ_PROY_INVITADO';
-if v_count > 0 then
-execute immediate 'drop user CMZ_PROY_INVITADO cascade';
-dbms_output.put_line('Usuario CMZ_PROY_INVITADO eliminado');
-else
-dbms_output.put_line('El usuario CMZ_PROY_INVITADO no existe, no se requiere eliminar');
+  select count(*) into v_count
+  from dba_users
+  where username ='CMZ_PROY_INVITADO';
+  if v_count > 0 then
+    execute immediate 'drop user CMZ_PROY_INVITADO cascade';
+    dbms_output.put_line('Usuario CMZ_PROY_INVITADO eliminado');
+  else
+    dbms_output.put_line('El usuario CMZ_PROY_INVITADO no existe, no se requiere eliminar');
 end if;
 end;
 /
