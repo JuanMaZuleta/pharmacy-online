dbfd6f255e8c0df0130bbacfc8f3d03e92ee4c6c
pedroalvarez <pedro@pc-pma.fi.unam>
2022-01-05 23:06:40 -0600

    modificacion de atriputos de apellidos

--- s-15-fx-clave_cliente.sql
+++ s-15-fx-clave_cliente.sql
@@ -1,8 +1,8 @@
 /*Función para construir una clave para cada cliente. 
-Las claves se deberán formar por el prefijo "CL-"seguido de las 2 primeras letras del CURP, 
+Las claves estàn compuetas por el prefijo "CL-"seguido de las 2 primeras letras del CURP, 
 seguido de las 2 últimas letras del apellido materno y por la primera letra del nombre.
-La clave debe estar formada únicamente por caracteres en mayúsculas. 
-Algunos clientes no cuentan con CURP. Modificar de tal forma que, si el CURP no existe, 
+La clave esta formada únicamente por caracteres en mayúsculas. 
+Algunos clientes no cuentan con CURP. Si el CURP no existe, 
 se haga uso de la cadena ‘00’.*/
 
 create or replace function clave_cliente(
@@ -13,7 +13,7 @@ create or replace function clave_cliente(
 begin 
   select upper('CL-' 
     || nvl(substr(curp,1,2),'00')
-    || substr(apellido_materno,length(apellido_materno)-1,2)
+    || substr(ap_materno,length(ap_materno)-1,2)
     || substr(nombre,1,1)
   ) clave
   into v_clave
