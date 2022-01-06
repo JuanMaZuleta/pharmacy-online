def837c60a9351fad03a4a57ba569e100fe9f87d
Pedro Martinez <pedroomtz17@gmail.com>
2022-01-05 19:50:19 -0600

    script para generar consultas

---
+++ s-10-consultas.sql
@@ -0,0 +1,18 @@
+/*1. Seleccionar los pedidos que tienen el status de cancelado
+y mostrar como el detalle de dichos pedidos.*/
+
+
+
+/*2. Generar un reporte que muestre el nombre, presentacion y precio de 
+los medicamentos comprados por el usuario #########.*/
+
+
+/*3.Seleccionar el total de los ingresos obtenidos del año 2020*/
+
+
+/*4.Generar un reporte que muestre el id y nombre de los empleados que son responsables 
+de los pedidos que tienen el stutas de cancelado, así como el detalle de cada uno de 
+los pedidos.
+
+
+
