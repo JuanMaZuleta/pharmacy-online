c847f05cdcf7147c5cffabc1f145bffeaa33385b
JuanMaZuleta <36617326+Zulce@users.noreply.github.com>
2022-01-06 19:48:06 -0600

    índices completos (revisar en conjunto)

--- s-06-indices.sql
+++ s-06-indices.sql
@@ -31,3 +31,41 @@ create index historico_status_pedido_id_ix on historico_status(pedido_id);
 create index detalle_pedido_farmacia_id_ix on detalle_pedido(farmacia_id);
 create index detalle_pedido_medicamento_pres_id_ix on detalle_pedido(medicamento_presentacion_id);
 create index detalle_pedido_pedido_id_ix on detalle_pedido(pedido_id);
+
+--Índices en tabla CENTRO_DE_OPERACION
+create unique index centro_operacion_clave_iuk on centro_de_operacion(clave);
+
+--Índices en tabla FARMACIA
+create unique index farmacia_rfc_fiscal_iuk on farmacia(rfc_fiscal);
+create index farmacia_gerente_id_ix on farmacia(gerente_id);
+
+--Índices en tabla ALMACEN
+create index almacen_almacen_contigencia_id_ix on almacen(almacen_contigencia_id);
+--create index alamcen_capacidad_ix on almacen(max(capacidad)); Si existe consulta
+
+--Índices en tabla OFICINA
+create index oficina_nombre_ix on oficina(upper(nombre));
+--create unique index oficina_clave_presupuestal_iuk on oficina(clave_presupuestal);
+
+--Índices en tabla OPERACION
+create index operacion_almacen_id_ix on operacion(almacen_id);
+create index operacion_responsable_id_ix on operacion(responsable_id);
+--create index operacion_fecha_ix on operacion(to_char(fecha,'dd/mm/yyyy')); solo si exite una consulta
+
+--Índices en tabla MEDICAMENTO_OPERACION
+create index medicamento_operacion_almacen_id_ix on medicamento_operacion(almacen_id);
+create index medicamento_operacion_operacion_id_ix on medicamento_operacion(operacion_id);
+--create index medicamento_operacion_unidades_ix on medicamento_operacion(max(unidades)); Si existe consulta
+
+--Índices en tabla FARMACIA_MEDICAMENTO_PRESENTACION
+--create index farmacia_medicamento_presentacion_unidades_ix on farmacia_medicamento_presentacion(max(unidades)); Si existe consulta
+
+--Índices en tabla MEDICAMENTO_PRESENTACION
+create index medicamento_presentacion_medicamento_id_ix on medicamento_presentacion(medicamento_id);
+
+--Índices en tabla MEDICAMENTO
+--create index medicamento_precio_ix on medicamento(avg(precio)); Si existe consulta
+
+--Índices MEDICAMENTO_NOMBRE
+create index medicamento_nombre_medicamento_id_ix on medicamento_nombre(medicamento_id);
+create index medicamento_nombre_nombre_ix on medicamento_nombre(upper(nombre));
