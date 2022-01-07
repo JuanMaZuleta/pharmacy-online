29a1bdb4aff51c087fe5f16a2074e5aaf4cbf285
pedroalvarez <pedro@pc-pma.fi.unam>
2022-01-07 00:59:59 -0600

    cambios a funcion de importe

--- s-15-fx-importe.sql
+++ s-15-fx-importe.sql
@@ -1,7 +1,7 @@
 --Función para obtener el importe de un pedido
 create or replace function importe(
-  p_folio number
-) return number is
+  p_folio varchar2
+) return varchar2 is
   --Declaracion de variables
   v_importe pedido.importe%type;
 begin 
@@ -11,9 +11,9 @@ begin
   join detalle_pedido dp 
     on p.pedido_id = dp.pedido_id
   join medicamento_presentacion mp 
-    on mp.medicamento_presentacion_id = dp.detalle_pedido
-  join medicamento
-    on m.medicamento_id = mp.medicamento_presentacion
+    on mp.medicamento_presentacion_id = dp.medicamento_presentacion_id
+  join medicamento m
+    on m.medicamento_id = mp.medicamento_id
   where p.folio =  p_folio;
   return v_importe;
 end;
