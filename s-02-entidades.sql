d0a65665908f68f498bc45c390d468c3ba804f11
pedroalvarez <pedro@pc-pma.fi.unam>
2022-01-07 22:31:52 -0600

    agregacion de atributo medicamento.es_riesgo

--- s-02-entidades.sql
+++ s-02-entidades.sql
@@ -94,7 +94,7 @@ create table medicamento (
 	medicamento_id number(10,0) constraint medicamento_id primary key,
 	sustancia_activa 	varchar2(400)	not null,
 	descripcion	varchar2(400) not null,
-	es_riego  number(1,0) not null
+	es_riesgo  number(1,0) not null
 );
 
 Prompt tabla medicamento_nombre
