8f3744d98979ec60c1fa640c1c517c2ac721b46c
JuanMaZuleta <36617326+Zulce@users.noreply.github.com>
2022-01-10 12:35:35 -0600

    Update s-13-p-actualiza-distribucion.sql

--- s-13-p-actualiza-distribucion.sql
+++ s-13-p-actualiza-distribucion.sql
@@ -21,6 +21,7 @@ set serveroutput on
 create or replace procedure sp_inserta_plano
 (p_almacen_id in number, p_nombre_archivo in varchar2) is
 
+--Deckaración de variables para la carga del archivo
 v_bfile bfile;
 v_src_offset number;
 v_dest_offset number;
@@ -30,27 +31,34 @@ v_dest_length number;
 
 begin
 dbms_output.put_line('Cargando plano '||p_nombre_archivo);
---Validando si el archivo existe
+
+--Validando si el archivo existe y asignando el archivo externo a v_bfile
 v_bfile:=bfilename('PLANOS_DIR',p_nombre_archivo);
 if dbms_lob.fileexists(v_bfile)=0 then
 	raise_application_error(-20001,'El archivo '||p_nombre_archivo||' no existe.');
 end if;
---abrir archivo
+
+--Verificando si el archivo está abierto
 if dbms_lob.isopen(v_bfile)=1 then
 	raise_application_error(-20001,'El archivo '||p_nombre_archivo||' está abierto. No se puede usar');
 end if;
+
 --abriendo archivo 
 dbms_lob.open(v_bfile,dbms_lob.lob_readonly);
+
 --Actualizando blob en la tabla
---Asegurarse que la tabla auto contenga datos y la columna foto debe tener un blob vacío.
+--Asegurarse que la tabla alamcen contenga datos y la columna distribucion debe tener un blob vacío.
 --Asignar v_blob
 select distribucion into v_blob
 from almacen
 where almacen_id=p_almacen_id
 for update;
---Escribiendo bytes
+
+--Definición de bytes de inicio para la lectura
 v_src_offset:=1;
 v_dest_offset:=1;
+
+--Carga de archivo con paquete dbms_lob
 dbms_lob.loadblobfromfile(
 	dest_lob      => v_blob,
 	src_bfile     => v_bfile,
@@ -58,12 +66,15 @@ dbms_lob.loadblobfromfile(
 	dest_offset   => v_dest_offset,
 	src_offset    => v_src_offset
     );
+    
 --Cerrando archivo
 dbms_lob.close(v_bfile);
+
 --Validando carga
 v_src_length:=dbms_lob.getlength(v_bfile);
 v_dest_length:=dbms_lob.getlength(v_blob);
-    
+ 
+ --Corroborando si carga se realizó adecuadamente 
 if v_dest_length<>v_src_length then 
 	raise_application_error(-20001,'El archivo '||p_nombre_archivo||' no se cargó correctamente');
 end if;
