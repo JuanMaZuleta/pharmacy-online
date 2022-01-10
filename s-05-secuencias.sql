2b2203d5150a8b81df27c454af9931532ed27f70
Diego_A_Cruz <36640876+diego9808@users.noreply.github.com>
2022-01-09 19:46:54 -0600

    Nuevas secuencias e increment by 1

--- s-05-secuencias.sql
+++ s-05-secuencias.sql
@@ -5,72 +5,119 @@
 --@Descripción: Creaciòn de secuencias
 
 create sequence seq_empleado 
-start with 1001
-nocycle
-cache 20;
+  start with 1001
+  increment by 1 
+  nocycle
+  cache 20
+;
 
 create sequence seq_ubicacion
-start with 1001
-nocycle
-cache 20;
+  start with 1001
+  increment by 1
+  nocycle
+  cache 20
+;
 
 create sequence seq_pedido
-start with 1
-nocycle
-cache 20;
+  start with 1
+  increment by 1
+  nocycle
+  cache 20
+;
 
 create sequence seq_tarjeta
-start with 801
-nocycle
-cache 20;
+  start with 801
+  increment by 1
+  nocycle
+  cache 20
+;
 
 create sequence seq_cliente
-start with 1001
-nocycle
-cache 20;
+  start with 1001
+  increment by 1
+  nocycle
+  cache 20
+;
 
 create sequence seq_historico_status
-start with 1751
-nocycle
-cache 20;
+  start with 1751
+  increment by 1
+  nocycle
+  cache 20
+;
 
 create sequence seq_detalle_pedido
-start with 1001
-nocycle
-cache 6;
+  start with 1001
+  increment by 1
+  nominvalue
+  nomaxvalue
+  nocycle
+  cache 6
+;
 
 create sequence centro_de_operacion_seq
-start with 1001
-cache 20
-nocycle;
+  start with 1001
+  increment by 1
+  cache 20
+  nocycle
+;
 
 create sequence medicamento_presentacion_seq
-start with 1
-cache 20
-nocycle;
+  start with 1
+  cache 20
+  nocycle
+;
 
 create sequence medicamento_seq
-start with 1
-increment by 1
-cache 20
-nocycle;
+  start with 1
+  increment by 1
+  cache 20
+  nocycle
+;
 
 create sequence presentacion_seq
-start with 11
-cache 20
-nocycle;
+  start with 11
+  increment by 1
+  cache 20
+  nocycle
+;
 
 create sequence medicamento_nombre_seq
-start with 1001
-cache 20
-nocycle;
+  start with 1001
+  increment by 1
+  cache 20
+  nocycle
+;
 
 create sequence operacion_seq
-start with 1001
-cache 20
-nocycle;
+  start with 1001
+  increment by 1
+  cache 20
+  nocycle
+;
 
 create sequence medicamento_operacion_seq
-start with 1001
-cache 20
-nocycle;
+  start with 1001
+  increment by 1
+  cache 20
+  nocycle
+;
+
+create sequence empleado_desp_seq
+  start with 1
+  increment by 1
+  nominvalue
+  nomaxvalue
+  cache 20
+  noorder
+;
+
+create sequence operacion_riesgo_seq
+  start with 1
+  increment by 1
+  nominvalue
+  nomaxvalue
+  cache 20
+  noorder
+;
+
