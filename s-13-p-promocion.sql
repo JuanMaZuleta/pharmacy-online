0cd49a80ee64b1ad9009a9129174e0e1336e1411
JuanMaZuleta <36617326+Zulce@users.noreply.github.com>
2022-01-10 01:37:33 -0600

    Procedimiento que realiza una promocion

---
+++ s-13-p-promocion.sql
@@ -0,0 +1,42 @@
+--@Autores: Diego Cruz, Pedro Martínez, Manuel Zuleta
+--@Fecha creación: 02/01/2022
+--@Descripción: Creación de nueva orden 
+
+set serveroutput on
+create or replace procedure sp_promocion 
+(p_farmacia_id in number,p_unidades_regalo in number) is 
+
+cursor cur_promocion is 
+select dp.unidades,p.status_pedido_id,m.es_riesgo,dp.medicamento_presentacion_id
+from detalle_pedido dp
+join pedido p 
+on dp.pedido_id=p.pedido_id
+join medicamento_presentacion mp
+on mp.medicamento_presentacion_id=dp.medicamento_presentacion_id
+join medicamento m 
+on mp.medicamento_id=m.medicamento_id;
+
+v_undidades_regalo number;
+v_sql_promocion varchar2(4000);
+
+begin
+for r in cur_promocion loop
+	if p_unidades_regalo > 5 then 
+		 raise_application_error(-20001,'La cantidad de medicamentos de regalo '
+		 								||p_unidades_regalo
+										||' es exesiva. Sólo se regalan entre 1 a 5 unidades. ');
+	end if;
+
+	v_undidades_regalo:=r.unidades + p_unidades_regalo;
+
+	if r.status_pedido_id=1 and r.es_riesgo=0 then
+		v_sql_promocion:='update detalle_pedido'
+					   ||' set unidades=:ph_unidades_regalo'
+		    		 ||' where farmacia_id=:ph_farmacia_id and medicamento_presentacion_id=:ph_medicamento_presentacion_id';
+		execute immediate v_sql_promocion using v_undidades_regalo,p_farmacia_id,r.medicamento_presentacion_id;
+	end if;
+end loop;
+end;
+/ 
+show errors
+
