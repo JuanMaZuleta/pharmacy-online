eb71b804b5d5f05aa1ea30aa2b6f66b108ca1ecc
JuanMaZuleta <36617326+Zulce@users.noreply.github.com>
2022-01-10 15:21:31 -0600

    Update s-16-fx-clave-cliente-prueba.sql

--- s-16-fx-clave-cliente-prueba.sql
+++ s-16-fx-clave-cliente-prueba.sql
@@ -10,15 +10,20 @@ v_clave varchar2(20);
 v_email varchar(50);
 
 begin
+
+--Consulta para insertar parámetro email en la función
 select email into v_email
 from cliente 
 where cliente_id=1;
 
-dbms_output.put_line('Se generara la clave al cliente con id=1 e email: '
+--Se imprime la información del cliente al que se le generará una clave
+dbms_output.put_line('Se generara la clave al cliente con id= 1 e email: '
 		||v_email);
 
+--Se aplica la función 
 select clave_cliente(v_email) into v_clave from dual;
 
+--Se imprime el valor de la clave generada 
 dbms_output.put_line('La clave para el cliente es: '
 		||v_clave);
 commit;
