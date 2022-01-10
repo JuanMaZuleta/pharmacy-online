7017ab4369f06f9802ca8dd2004667df017b4bb7
JuanMaZuleta <36617326+Zulce@users.noreply.github.com>
2022-01-10 11:13:03 -0600

    Create s-pruebas.sql

---
+++ s-pruebas.sql
@@ -0,0 +1,18 @@
+prompt ejecución de pruebas para triggers
+prompt prueba para tr-actual_unidades
+@s-12-tr-actual_unidades-prueba.sql
+
+prompt prueba para tr-empleado_despedido
+@s-12-tr-empleado_despedido-prueba.sql
+
+prompt prueba para p-nueva-orden
+@s-14-p-nueva-orden-prueba.sql
+
+prompt prueba para p-promocion
+@s-14-p-promocio-prueba.sql
+
+prompt prueba para p-carga-distribucion
+@s-14-p-actualiza-distribicion-prueba.sql
+
+prompt prueba para función clave_cliente
+@s-16-fx-clave-cliente-prueba.sql
