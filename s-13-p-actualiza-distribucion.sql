f30135513705b2b0a6e51081d5166f09474fc33e
JuanMaZuleta <36617326+Zulce@users.noreply.github.com>
2022-01-10 01:36:00 -0600

    Update s-13-p-actualiza-distribucion.sql

--- s-13-p-actualiza-distribucion.sql
+++ s-13-p-actualiza-distribucion.sql
@@ -23,44 +23,44 @@ v_src_length number;
 v_dest_length number;
 
 begin
-    dbms_output.put_line('Cargando plano '||p_nombre_archivo);
-    --Validando si el archivo existe
-    v_bfile:=bfilename('PLANOS_DIR',p_nombre_archivo);
-    if dbms_lob.fileexists(v_bfile)=0 then
-        raise_application_error(-20001,'El archivo '||p_nombre_archivo||' no existe.');
-    end if;
-    --abrir archivo
-    if dbms_lob.isopen(v_bfile)=1 then
-        raise_application_error(-20001,'El archivo '||p_nombre_archivo||' está abierto. No se puede usar');
-    end if;
-    --abriendo archivo 
-    dbms_lob.open(v_bfile,dbms_lob.lob_readonly);
-    --Actualizando blob en la tabla
-    --Asegurarse que la tabla auto contenga datos y la columna foto debe tener un blob vacío.
-    --Asignar v_blob
-    select distribucion into v_blob
-    from almacen
-    where almacen_id=p_almacen_id
-    for update;
-    --Escribiendo bytes
-    v_src_offset:=1;
-    v_dest_offset:=1;
-    dbms_lob.loadblobfromfile(
-        dest_lob      => v_blob,
-        src_bfile     => v_bfile,
-        amount        => dbms_lob.getlength(v_bfile),
-        dest_offset   => v_dest_offset,
-        src_offset    => v_src_offset
+dbms_output.put_line('Cargando plano '||p_nombre_archivo);
+--Validando si el archivo existe
+v_bfile:=bfilename('PLANOS_DIR',p_nombre_archivo);
+if dbms_lob.fileexists(v_bfile)=0 then
+	raise_application_error(-20001,'El archivo '||p_nombre_archivo||' no existe.');
+end if;
+--abrir archivo
+if dbms_lob.isopen(v_bfile)=1 then
+	raise_application_error(-20001,'El archivo '||p_nombre_archivo||' está abierto. No se puede usar');
+end if;
+--abriendo archivo 
+dbms_lob.open(v_bfile,dbms_lob.lob_readonly);
+--Actualizando blob en la tabla
+--Asegurarse que la tabla auto contenga datos y la columna foto debe tener un blob vacío.
+--Asignar v_blob
+select distribucion into v_blob
+from almacen
+where almacen_id=p_almacen_id
+for update;
+--Escribiendo bytes
+v_src_offset:=1;
+v_dest_offset:=1;
+dbms_lob.loadblobfromfile(
+	dest_lob      => v_blob,
+	src_bfile     => v_bfile,
+	amount        => dbms_lob.getlength(v_bfile),
+	dest_offset   => v_dest_offset,
+	src_offset    => v_src_offset
     );
-    --Cerrando archivo
-    dbms_lob.close(v_bfile);
-    --Validando carga
-    v_src_length:=dbms_lob.getlength(v_bfile);
-    v_dest_length:=dbms_lob.getlength(v_blob);
+--Cerrando archivo
+dbms_lob.close(v_bfile);
+--Validando carga
+v_src_length:=dbms_lob.getlength(v_bfile);
+v_dest_length:=dbms_lob.getlength(v_blob);
     
-    if v_dest_length<>v_src_length then 
-        raise_application_error(-20001,'El archivo '||p_nombre_archivo||' no se cargó correctamente');
-    end if;
+if v_dest_length<>v_src_length then 
+	raise_application_error(-20001,'El archivo '||p_nombre_archivo||' no se cargó correctamente');
+end if;
 end;
 /
 show errors
